e49b06b76e61a0d695c47a0eb55255d27c992f86
feichao <len.may@foxmail.com>
2017-03-21 21:38:45 +0800

    update demo

--- demo/demo.js
+++ demo/demo.js
@@ -3,14 +3,47 @@
 
   angular.module('MyApp', ['ngMaterial', 'ngMdIcons', 'ngDatetime'])
     .controller('MyController', function($scope) {
-      $scope.date = "2017-03-17";
-      $scope.time = "10:34:10";
-    	$scope.datetime = "2017-03-17 10:34:10";
+      var m = moment();
+      $scope.date = m.format('YYYY-MM-DD');
+      $scope.time = m.format('HH:mm:ss');
+    	$scope.datetime = m.format('YYYY-MM-DD HH:mm:ss');
 
-      $scope.max = '2017-03-29';
-      $scope.min = '2017-03-03';
+      $scope.startDate = m.format('YYYY-MM-DD');
+      $scope.endDate = moment(m).add(10, 'days').format('YYYY-MM-DD');
+
+      $scope.startTime = m.format('HH:mm:ss');
+      $scope.endTime = moment(m).add(10, 'minutes').format('HH:mm:ss');
+
+      $scope.startDatetime = m.format('YYYY-MM-DD HH:mm:ss');
+      $scope.endDatetime = moment(m).add(5, 'days').add(10, 'minutes').format('YYYY-MM-DD HH:mm:ss');
+
+      $scope.startDatetime0 = m.format('YYYY-MM-DD HH:mm:ss');
+      $scope.endDatetime0 = moment(m).add(10, 'minutes').format('YYYY-MM-DD HH:mm:ss');
+
+      $scope.max = moment(m).add(10, 'days').format('YYYY-MM-DD');
+      $scope.min = moment(m).add(-10, 'days').format('YYYY-MM-DD');
 
       $scope.qSelect = [{
+        label: '15 分钟前',
+        value: 15 * 60,
+      }, {
+        label: '30 分钟前',
+        value: 30 * 60,
+      }, {
+        label: '5 小时前',
+        value: 5 * 60 * 60,
+      }, {
+        label: '10 天前',
+        value: 10 * 24 * 60 * 60,
+      }, {
+        label: '30 天前',
+        value: 30 * 24 * 60 * 60,
+      }, {
+        label: '1 年前',
+        value: 365 * 24 * 60 * 60,
+      }];
+
+      $scope.qSelectRange = [{
         label: '最近 15 分钟',
         value: 15 * 60,
       }, {
@@ -22,9 +55,6 @@
       }, {
         label: '最近 10 天',
         value: 10 * 24 * 60 * 60,
-      }, {
-        label: '最近 20 天',
-        value: 20 * 24 * 60 * 60,
       }, {
         label: '最近 30 天',
         value: 30 * 24 * 60 * 60,
@@ -40,8 +70,11 @@
       };
 
       $scope.confirm = function(a, b) {
-        console.log('start: ' + a);
-        console.log('end: ' + b);
+        if(b) {
+          console.log('Your select: start is ' + a + ', end is ' + b);
+        } else {
+          console.log('Your select: ' + a);
+        }
       }
     });
 })();
